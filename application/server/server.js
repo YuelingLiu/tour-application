c82ffcc8baf67d6d8739be2e1d37132ed7a0d8a7
Yueling Liu <74637471+YuelingLiu@users.noreply.github.com>
2023-07-19 12:54:20 -0700

    clean up

--- application/server/server.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-const express = require('express');
-
-const app = express();
-app.use(express.json());
-
-const PORT = process.env.PORT || 8080;
-console.log('Hello world');
-app.listen(PORT, () => console.log(`Server listening in port ${PORT}`));
