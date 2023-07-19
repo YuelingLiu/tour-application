8bc55b1dbee41fbd39076ca33ef7463458920190
Yueling Liu <74637471+YuelingLiu@users.noreply.github.com>
2023-07-19 13:41:46 -0700

    env file

--- application/server/server.js
+++ application/server/server.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Tour app listening at http://localhost:${PORT}`);
+});
