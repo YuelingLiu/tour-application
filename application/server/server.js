22ec396d3ab73b94d94aecfd86ec0e36d170d93e
Yueling Liu <74637471+YuelingLiu@users.noreply.github.com>
2023-07-19 17:22:20 -0700

    connected to db

--- application/server/server.js
+++ application/server/server.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env' });
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Tour app listening at http://localhost:${PORT}`);
-});
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+    app.listen(PORT, () => {
+      console.log(`Tour app listening at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('DB connection failed:', err.message);
+  });
