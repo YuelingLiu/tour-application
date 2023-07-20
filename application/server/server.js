a6433c363615fbe669193b54db7790a7f0fd4208
Yueling Liu <74637471+YuelingLiu@users.noreply.github.com>
2023-07-20 11:35:23 -0700

    server is ok

--- application/server/server.js
+++ application/server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const tourRoute = require('./routes/tourRoute');
+
 dotenv.config({ path: '.env' });
 
+const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Connecting to the database
 const DB = process.env.DATABASE.replace(
   '<password>',
   process.env.DATABASE_PASSWORD
@@ -24,3 +30,8 @@ mongoose
   .catch((err) => {
     console.error('DB connection failed:', err.message);
   });
+
+app.get('/', (req, res) => res.send('Hello World from overview /'));
+
+// Mount the tourRoute as a middleware under the '/api/v2/tours' path
+app.use('/api/v1/tours', tourRoute);
