a76afa72e6b61fb3aa2254eba42caef78829b0e6
Yueling Liu <74637471+YuelingLiu@users.noreply.github.com>
2023-07-20 16:54:49 -0700

    dummy words on the site

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 function App() {
-  return <div></div>;
+  return <div>Welcome to our tours</div>;
 }
 
 export default App;
