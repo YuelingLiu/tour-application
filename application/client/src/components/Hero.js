824b23da33a4544a3b5c1a841f10f248db2fee3e
Yueling Liu <74637471+YuelingLiu@users.noreply.github.com>
2023-08-17 23:01:08 -0700

    landing page

--- application/client/src/components/Hero.js
+++ application/client/src/components/Hero.js
@@ -11,41 +11,79 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function Hero() {
   return (
-    <Container className="container">
-      <Row>
-        <Col xs={5} sm={12} md={5} xl={5} className="leftSide">
-          <Row className="d-flex justify-content-center">
-            <h1>Explore The World</h1>
-          </Row>
-          <Row>
-            <p>
-              No matter where in the world you want to go, we can help you get
-              there
-            </p>
-          </Row>
-          <Row>
-            <Col xs={12} sm={5} md={6} lg={4} xl={4}>
-              <Link to="/" style={{ textDecoration: 'none' }}>
-                <Button
-                  style={{ marginBottom: '10px' }}
-                  size="md"
-                  variant="success"
-                >
-                  Explore All Tours Now
-                </Button>
-              </Link>
-            </Col>
-          </Row>
-        </Col>
+    <Row style={{ marginTop: '10px' }}>
+      <Col
+        xs={5}
+        sm={12}
+        md={4}
+        xl={5}
+        className="d-flex flex-column justify-content-center align-items-center  custom-col-center"
+      >
+        <Row className="d-flex flex-column justify-content-center">
+          <h1 style={{ fontFamily: 'initial', fontSize: '50px' }}>
+            Explore The World
+          </h1>
+        </Row>
 
-        {/* img (right side) */}
-        <Col xs={7} sm={12} md={7} xl={7}>
-          <div className="full-width-image-container">
-            <img className="full-width-image" src="hero.jpeg" alt="hero" />
-          </div>
-        </Col>
-      </Row>
-    </Container>
+        <Row>
+          <p
+            style={{
+              fontFamily: 'initial',
+              fontSize: '25px',
+            }}
+          >
+            No matter where in the world you want to go, we can help you get
+            there
+          </p>
+        </Row>
+
+        <Row>
+          {/* Add this line */}
+          <Col xs={12} sm={5} md={6} lg={4} xl={4}>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+              <Button
+                style={{
+                  marginBottom: '10px',
+                  width: '150px',
+                  height: '100%',
+                  fontWeight: 'bold',
+                }}
+                size="lg"
+                variant="success"
+              >
+                All Tours
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col
+            xs={7}
+            sm={12}
+            md={7}
+            xl={7}
+            style={{ marginTop: '100px' }}
+            className="mx-auto text-center"
+          >
+            <div
+              style={{ margin: '0 auto' }}
+              className="full-width-image-container"
+            >
+              <img className="full-width-image" src="hero1.jpeg" alt="hero" />
+            </div>
+          </Col>
+        </Row>
+      </Col>
+
+      {/* img (right side) */}
+
+      <Col xs={5} sm={12} md={9} xl={7}>
+        <div className="full-width-image-container">
+          <img className="full-width-image" src="hero.jpeg" alt="hero" />
+        </div>
+      </Col>
+    </Row>
   );
 }
 
--- application/client/src/components/MyNavbar.js
+++ application/client/src/components/MyNavbar.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Nav, Navbar } from 'react-bootstrap';
 import '../styling/nav.css'; // Importing the CSS file
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const MyNavbar = () => {
+function MyNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">Tour-Vista</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/signup">Signup</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar
+      className="custom-navbar"
+      variant="dark"
+      expand="lg"
+      style={{ padding: '20px' }}
+    >
+      <Navbar.Brand
+        href="/"
+        style={{
+          fontSize: '24px',
+          fontWeight: 'bold',
+          marginLeft: '10px',
+          color: '#333333',
+        }}
+      >
+        TOUR-VISTA
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link
+            href="/login"
+            style={{
+              fontSize: '20px',
+              marginRight: '10px',
+              fontWeight: 'bold',
+              color: '#333333',
+            }}
+          >
+            Login
+          </Nav.Link>
+          <Nav.Link
+            href="/register"
+            style={{ fontSize: '20px', fontWeight: 'bold', color: '#333333' }}
+          >
+            Register
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
-};
+}
 
 export default MyNavbar;
