664311269579331858911670b15408944d01ffdf
Yueling Liu <74637471+YuelingLiu@users.noreply.github.com>
2023-08-25 09:16:46 -0700

    clean up userContoller

--- application/server/controllers/userController.js
+++ application/server/controllers/userController.js
@@ -28,14 +28,12 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-<<<<<<< Updated upstream
-
 // get one user using ID
-exports.getUser = async(req, res) => {
+exports.getUser = async (req, res) => {
   console.log('in getUser function');
   try {
     console.log('req.params.id', req.params);
-    // wait for user to be found 
+    // wait for user to be found
     const user = await User.findById(req.params.id);
 
     // check if user found is a success or not
@@ -44,43 +42,31 @@ exports.getUser = async(req, res) => {
     }
 
     // if found send status and data
-    res.status(200).json({status: 'success', data: { user }});
+    res.status(200).json({ status: 'success', data: { user } });
   } catch (err) {
-    res.status(400).json({status: 'fail', message: err.message});
+    res.status(400).json({ status: 'fail', message: err.message });
   }
-}
-
+};
 
 // delete user
-exports.deleteUser = async (req, res) => { 
+exports.deleteUser = async (req, res) => {
   console.log('in delete function');
   try {
     // get user by id
     const user = await User.findByIdAndDelete(req.params.id);
-  
+
     // check if user found is a success or not
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
-  
-    // if success send message 
-    res.status(200).json({status: 'success', data: null})
+
+    // if success send message
+    res.status(200).json({ status: 'success', data: null });
   } catch (err) {
-    res.status(400).json({status: 'fail', message: err.message});
+    res.status(400).json({ status: 'fail', message: err.message });
   }
-}
+};
 
-
-// update user 
-
-// exports.createUser = (req, res) => {
-//   console.log(req.requestTime);
-//   res.status(500).json({
-//     status: 'error',
-//     message: 'This route is not defined! Please use /signup',
-//   });
-// };
-=======
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -116,4 +102,3 @@ exports.createUser = (req, res) => {
     message: 'This route is not defined! Please use /signup',
   });
 };
->>>>>>> Stashed changes
